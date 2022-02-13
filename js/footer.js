1b346525b084e8a4e0c2d61b39a81aa5f827fde7
robbradley5678 <robbradley5678@yahoo.com>
2022-02-13 11:54:10 +0000

    Update copyright year

--- js/footer.js
+++ js/footer.js
@@ -1,6 +1,7 @@
 const footer = document.getElementsByTagName('footer')[0];
+const year = new Date().getFullYear();
 footer.innerHTML = `
 
-  <p>&#169; Robert Bradley 2021 | <a href="./termsandconditions.html" target="_blank">Terms & Conditions</a> | <a href="./privacy.html" target="_blank">Privacy Policy</a></p>
+  <p>&#169; Robert Bradley 2022 | <a href="./termsandconditions.html" target="_blank">Terms & Conditions</a> | <a href="./privacy.html" target="_blank">Privacy Policy</a></p>
 
 `;
